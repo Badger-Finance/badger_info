65ba53309a601dce404f28549c80ca359b8cb4b7
Umair Darr <umerderr@gmail.com>
2021-06-30 15:33:32 +0100

    add heroku endpoint for boosts

--- src/state/boosts/updater.ts
+++ src/state/boosts/updater.ts
@@ -6,12 +6,15 @@ export default function Updater() {
   const updateBoostData = useUpdateBoostData()
   const boostData = useBoostData()
   useEffect(() => {
-    if (boostData.length == 0) {
-      const { error, data } = fetchBoosts()
-      if (!error) {
-        updateBoostData(data)
+    async function update() {
+      if (boostData.length == 0) {
+        const { error, data } = await fetchBoosts()
+        if (!error) {
+          updateBoostData(data)
+        }
       }
     }
+    update()
   }, [])
   return null
 }
