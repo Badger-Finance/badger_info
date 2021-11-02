9ae90cff60ff3d9373a14ca1ba83cd2f334b8b9c
Umair Darr <umerderr@gmail.com>
2021-11-02 20:05:34 +0000

    feat: fix header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -244,9 +244,11 @@ export default function Header() {
           </BadgerIcon>
         </Title>
         <HeaderLinks>
-          {<StyledNavLink id={`pool-nav-link`} to={'/cycles'}>
-            Cycles
-          </StyledNavLink>}
+          {
+            <StyledNavLink id={`pool-nav-link`} to={'/cycles'}>
+              Cycles
+            </StyledNavLink>
+          }
 
           <StyledNavLink id={`pool-nav-link`} to={'/schedules'}>
             Schedules
