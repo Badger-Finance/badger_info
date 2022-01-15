3e1fa2f930575127182154e1b8f60abb53c2a20a
Umair Darr <umerderr@gmail.com>
2022-01-15 16:51:13 +0000

    feat: update constants

--- src/constants/nfts.ts
+++ src/constants/nfts.ts
@@ -9,7 +9,7 @@ const nfts: any = {
   '0xe4605d46fd0b3f8329d936a8b258d69276cba264-205': { name: 'Badger Digg: Wack-A-Badger', value: 50 },
   '0xe4605d46fd0b3f8329d936a8b258d69276cba264-206': { name: 'Badger Digg: Badgerpack Joyride', value: 200 },
   '0xe4605d46fd0b3f8329d936a8b258d69276cba264-208': { name: 'Badger Digg: Battle Badger', value: 1000 },
-  '0xe4605d46fd0b3f8329d936a8b258d69276cba264-99': { name: 'Badger Pizza', value: 10 },
+  '0xe4605d46fd0b3f8329d936a8b258d69276cba264-99': { name: 'Badger Pizza', value: 50 },
   '0xe4605d46fd0b3f8329d936a8b258d69276cba264-98': { name: 'HODL', value: 10 },
   '0xe4605d46fd0b3f8329d936a8b258d69276cba264-97': { name: 'Mt Gox', value: 10 },
 }
